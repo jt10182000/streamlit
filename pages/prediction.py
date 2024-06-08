d97fc66784088bd9f4b84059c883c5f76c4265aa
Gelluh <114986828+Gelluh@users.noreply.github.com>
2024-06-08 04:31:15 -0700

    prediction.py

--- pages/prediction.py
+++ pages/prediction.py
@@ -4,12 +4,12 @@
 # python -m streamlit run streamlit_test.py
 
 import streamlit as st
+import pandas as pd
 import pickle
 
 # Load the trained Naive Bayes classifier from the saved file
 filename = 'pages/sentimentAnalyzerTest_Model.sav'
-with open(filename, 'rb') as file:
-    loaded_model = pickle.load(file)
+loaded_model = pickle.load(open(filename, 'rb'))
 
 st.title("Flood Cause Predictor :umbrella:")
 st.subheader("Enter levels of different factors to determine the potential cause of the flood:")
@@ -17,20 +17,15 @@ st.subheader("Enter levels of different factors to determine the potential cause
 # User inputs for different factors
 urbanization_input = st.slider("Urbanization Level: ", 0, 500)
 deforestation_input = st.slider("Deforestation Level: ", 0, 500)
-agricultural_practices_input = st.slider("Agricultural Practices Level: ", 0, 500)
+agricultural_practices_input = st.slider("Intensity of Agricultural Practices: ", 0, 500)
 
 # Function to make a prediction
 def predict_flood_cause(urbanization, deforestation, agriculture):
     if urbanization == 0 and deforestation == 0 and agriculture == 0:
         return "No significant factors entered"
     else:
-        features = {
-            'urbanization': urbanization,
-            'deforestation': deforestation,
-            'agriculture': agriculture
-        }
-        prediction = loaded_model.classify(features)
-        return prediction
+        prediction = loaded_model.predict([[urbanization, deforestation, agriculture]])
+        return prediction[0]
 
 # Display button and result
 if st.button('Predict'):
