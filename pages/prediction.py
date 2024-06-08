7cba24dfa3a5c319258be22fd1dc1b0e369564b0
Gelluh <114986828+Gelluh@users.noreply.github.com>
2024-06-08 05:15:28 -0700

    prediction.py

--- pages/prediction.py
+++ pages/prediction.py
@@ -1,33 +1,33 @@
-import streamlit as st
+#Notes
+# do a "pip install streamlit" first 
+#to run on terminal issue this command
+# python -m streamlit run streamlit_test.py
 
-
-st.header('Simple Sentiment Analyzer App')
-st.subheader('This python code is implemented for Streamlit')
 import streamlit as st
 import pandas as pd
 import pickle
 from nltk.corpus import names
 
-st.title("A Simple Sentiment Analyzer")
-message = st.text_input("Tell me what you feel today: ")
-
-        # Load the trained Naive Bayes classifier from the saved file
-filename = 'pages/sentimentAnalyzerTest_model.sav'
+# Load the trained Naive Bayes classifier from the saved file
+filename = 'pages/crop_recom_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-        # Define features (words) and their corresponding labels (positive/negative)
-def word_features(words):
-        return dict([(word, True) for word in words])
+# # Use the model to make predictions
+@st.cache_data 
+def predict_crop():
+    st.text("The crop is " + crop_name)
+    return
+           
+st.title("Crop Recommendation Predictor :smile:")
+st.subheader("Enter a set of NPK levels to determine what crop best fits:")
+n_input = st.slider("Nitrogen: ",0,500)
+p_input = st.slider("Phosphorus: ",0,500)
+k_input = st.slider("Potassium: ",0,500)
+if n_input == 0 & p_input == 0 & k_input == 0:
+    crop_name = ""
+else:
+    crop_name = loaded_model.predict([[pd.to_numeric(n_input),pd.to_numeric(p_input),pd.to_numeric(k_input)]])
 
-message_tone = loaded_model.classify(word_features(message.split()))
-
-        # make a function for your button click
-
-def sayFeeling():
-            # Classify the sentiment
-        if message_tone == 'positive':
-           st.write("this is :smile:")
-        else:
-           st.write("this is :disappointed:")
-                
-st.button('Say it', on_click=sayFeeling)
+st.text("The crop suitable for this NPK level:")
+st.text_area(label ="",value=crop_name, height =100)
+# st.button('Predict', on_click=predict_crop
