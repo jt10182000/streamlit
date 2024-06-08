673e8dcfe36965c89fc1c8d562e0224db3374c3d
Gelluh <114986828+Gelluh@users.noreply.github.com>
2024-06-08 04:28:40 -0700

    prediction.py

--- pages/prediction.py
+++ pages/prediction.py
@@ -15,27 +15,25 @@ st.title("Flood Cause Predictor :umbrella:")
 st.subheader("Enter levels of different factors to determine the potential cause of the flood:")
 
 # User inputs for different factors
-rainfall_input = st.slider("Rainfall Level (mm): ", 0, 1000)
-river_overflow_input = st.slider("River Overflow Level: ", 0, 100)
-drainage_quality_input = st.slider("Drainage System Quality (1-10): ", 1, 10)
-dam_condition_input = st.slider("Dam Condition (1-10): ", 1, 10)
+urbanization_input = st.slider("Urbanization Level: ", 0, 500)
+deforestation_input = st.slider("Deforestation Level: ", 0, 500)
+agricultural_practices_input = st.slider("Agricultural Practices Level: ", 0, 500)
 
 # Function to make a prediction
-def predict_flood_cause(rainfall, river_overflow, drainage_quality, dam_condition):
-    if rainfall == 0 and river_overflow == 0 and drainage_quality == 1 and dam_condition == 1:
+def predict_flood_cause(urbanization, deforestation, agriculture):
+    if urbanization == 0 and deforestation == 0 and agriculture == 0:
         return "No significant factors entered"
     else:
         features = {
-            'rainfall': rainfall,
-            'river_overflow': river_overflow,
-            'drainage_quality': drainage_quality,
-            'dam_condition': dam_condition
+            'urbanization': urbanization,
+            'deforestation': deforestation,
+            'agriculture': agriculture
         }
         prediction = loaded_model.classify(features)
         return prediction
 
 # Display button and result
 if st.button('Predict'):
-    cause_of_flood = predict_flood_cause(rainfall_input, river_overflow_input, drainage_quality_input, dam_condition_input)
+    cause_of_flood = predict_flood_cause(urbanization_input, deforestation_input, agricultural_practices_input)
     st.text("The predicted cause of the flood is:")
     st.text_area(label="", value=cause_of_flood, height=100)
